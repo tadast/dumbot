be179e7648cfc01db459558f9addf6eb9595ef21
Tadas Tamosauskas <tadastoo@yahoo.com>
2011-10-27 17:16:35 +0100

    add more meme matchers

--- meme_bot.rb
+++ meme_bot.rb
@@ -12,13 +12,13 @@ end
 scamp.behaviour do
   # matches Jenkins notifier success message https://github.com/thickpaddy/jenkins_campfire_plugin
   match /(.)*SUCCESS(.)*/ do
-    say "http://ragefac.es/faces/7c5b930e2a57df597acf02f4bea0e252.png"
+    # say "http://ragefac.es/faces/7c5b930e2a57df597acf02f4bea0e252.png"
     play "yeah"
   end
 
   # matches Jenkins notifier failure message https://github.com/thickpaddy/jenkins_campfire_plugin
   match /(.)*FAILURE(.)*/ do
-    say "http://ragefac.es/faces/b6e647d23bf1c62c0cd8f7fe98a42823.png"
+    # say "http://ragefac.es/faces/b6e647d23bf1c62c0cd8f7fe98a42823.png"
     play "drama"
   end
 
@@ -47,13 +47,65 @@ scamp.behaviour do
   end
 
   match /^[f]+[u]+$/ do
-    say "http://ragefac.es/faces/medium_b801183d49d9a7a5491df449d78bceb3.png"
+    links = [
+      "http://ragefac.es/faces/medium_b801183d49d9a7a5491df449d78bceb3.png",
+      "http://s3.amazonaws.com/ragefaces/02094b42ed51e063b0b9a25e9b774850.png",
+      "http://s3.amazonaws.com/ragefaces/4f767454e72cef87d679c12823892356.png",
+      "http://s3.amazonaws.com/ragefaces/615fdaf29bbe5c29e6ac88e94328097d.png"
+    ]
+    say links.shuffle.first
+  end
+
+  match /(.)*whew(.)*/ do
+    say "http://s3.amazonaws.com/ragefaces/e1f11b7e02abf942f13564a72220ff34.png"
+  end
+
+  match /(.)*kids(.)*/ do
+    say "http://s3.amazonaws.com/ragefaces/a0bb256993d98fb2d037a0eac5a6b59c.png"
+    say "who said kids?"
+  end
+
+  match /(.)*do next?(.)*/ do
+    images = [
+      "http://1.bp.blogspot.com/_D_Z-D2tzi14/TBpOnhVqyAI/AAAAAAAADFU/8tfM4E_Z4pU/s400/responsibility12(alternate).png",
+      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOglLvLgI/AAAAAAAADFM/I7_IUXh6v1I/s400/responsibility10.png",
+      "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpOY-GY8TI/AAAAAAAADFE/eboe6ItMldg/s400/responsibility11.png",
+      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOOgiDnVI/AAAAAAAADE8/wLkmIIv-xiY/s400/responsibility13(alternate).png",
+      "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpa3lAAFQI/AAAAAAAADFs/8IVZ-jzQsLU/s400/responsibility14.png",
+      "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpoOlpMa_I/AAAAAAAADGU/CfZVMM9MqsU/s400/responsibility102.png",
+      "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpoVLLDgCI/AAAAAAAADGc/iqux8px_V-s/s400/responsibility12(alternate)2.png",
+      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpqGvZ7jVI/AAAAAAAADGk/hDTNttRLLks/s400/responsibility8.png"
+    ]
+    say images.shuffle.first
+  end
+
+  match /ship it!/ do
+    squirrels = [
+      "http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg",
+      "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png"
+    ]
+    say squirrels.shuffle.first
+  end
+
+  match /(.)*like a boss(.)*/ do
+    images = [
+      "http://s3.amazonaws.com/kym-assets/photos/images/original/000/114/151/14185212UtNF3Va6.gif?1302832919",
+      "http://s3.amazonaws.com/kym-assets/photos/images/newsfeed/000/110/885/boss.jpg",
+      "http://verydemotivational.files.wordpress.com/2011/06/demotivational-posters-like-a-boss.jpg",
+      "http://assets.head-fi.org/b/b3/b3ba6b88_funny-facebook-fails-like-a-boss3.jpg",
+      "http://img.anongallery.org/img/6/0/like-a-boss.jpg",
+      ]
+    say images.shuffle.first
+  end
+
+  # only if you run it locally
+  match /my ip?/ do
+    say "#{`wget -qO- icanhazip.com`}"
   end
 
   # useful stuff
 
   match /^geminfo (?<gemname>.+)/ do
-    say "hang on, looking for #{gemname} info..."
     begin
       response = Net::HTTP.get(URI.parse("http://rubygems.org/api/v1/versions/#{gemname}.json"))
       gem_info = JSON.parse(response).to_a
@@ -70,8 +122,8 @@ scamp.behaviour do
       response = Net::HTTP.get(URI.parse("http://api.twitter.com/1/users/show.json?screen_name=#{someone}"))
       json = JSON.parse(response)
       say "http://twitter.com/#!/#{someone}/status/#{json['status']['id_str']}"
-    rescue
-      say "pfffch pfffch"
+    rescue e
+      say "Error #{e}"
     end
   end
 
