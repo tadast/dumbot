4f49f3c2d2ffd34d35321644f79d8f599918f82d
Tadas Tamošauskas <tadas@pdfcv.com>
2012-01-29 16:36:40 +0000

    suggest a random task

--- bot.rb
+++ bot.rb
@@ -166,20 +166,6 @@ scamp.behaviour do
     say "who said children?"
   end
 
-  match /(.)*do next?(.)*/ do
-    images = [
-      "http://1.bp.blogspot.com/_D_Z-D2tzi14/TBpOnhVqyAI/AAAAAAAADFU/8tfM4E_Z4pU/s400/responsibility12(alternate).png",
-      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOglLvLgI/AAAAAAAADFM/I7_IUXh6v1I/s400/responsibility10.png",
-      "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpOY-GY8TI/AAAAAAAADFE/eboe6ItMldg/s400/responsibility11.png",
-      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOOgiDnVI/AAAAAAAADE8/wLkmIIv-xiY/s400/responsibility13(alternate).png",
-      "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpa3lAAFQI/AAAAAAAADFs/8IVZ-jzQsLU/s400/responsibility14.png",
-      "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpoOlpMa_I/AAAAAAAADGU/CfZVMM9MqsU/s400/responsibility102.png",
-      "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpoVLLDgCI/AAAAAAAADGc/iqux8px_V-s/s400/responsibility12(alternate)2.png",
-      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpqGvZ7jVI/AAAAAAAADGk/hDTNttRLLks/s400/responsibility8.png"
-    ]
-    say images.sample
-  end
-
   match /ship it!/ do
     squirrels = [
       "http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg",
@@ -249,6 +235,10 @@ scamp.behaviour do
 
   match /^task help$/ do
     say Tasker.instance.help
+  end
+
+  match /^random task( please)?$/i do
+    say Tasker.instance.random
   end
 
 end
--- lib/tasker.rb
+++ lib/tasker.rb
@@ -22,7 +22,7 @@ class Tasker
 
   def all_tasks
     if @tasks.empty?
-      "Nothing urgent."
+      nothing_urgent
     else
       @tasks.map do |task|
         "#{task} (#{task.owner || 'unclaimed'})"
@@ -36,7 +36,7 @@ class Tasker
       if @tasks.size > 0
         "Nothing assigned to you. Grab one of the #{@tasks.count} other tasks."
       else
-        "Nothing urgent."
+        nothing_urgent
       end
     else
       tasks.map(&:to_s).join("\n")
@@ -91,6 +91,18 @@ class Tasker
     end
   end
 
+  def random
+    [ "http://1.bp.blogspot.com/_D_Z-D2tzi14/TBpOnhVqyAI/AAAAAAAADFU/8tfM4E_Z4pU/s400/responsibility12(alternate).png",
+      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOglLvLgI/AAAAAAAADFM/I7_IUXh6v1I/s400/responsibility10.png",
+      "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpOY-GY8TI/AAAAAAAADFE/eboe6ItMldg/s400/responsibility11.png",
+      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpOOgiDnVI/AAAAAAAADE8/wLkmIIv-xiY/s400/responsibility13(alternate).png",
+      "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpa3lAAFQI/AAAAAAAADFs/8IVZ-jzQsLU/s400/responsibility14.png",
+      "http://3.bp.blogspot.com/_D_Z-D2tzi14/TBpoOlpMa_I/AAAAAAAADGU/CfZVMM9MqsU/s400/responsibility102.png",
+      "http://4.bp.blogspot.com/_D_Z-D2tzi14/TBpoVLLDgCI/AAAAAAAADGc/iqux8px_V-s/s400/responsibility12(alternate)2.png",
+      "http://2.bp.blogspot.com/_D_Z-D2tzi14/TBpqGvZ7jVI/AAAAAAAADGk/hDTNttRLLks/s400/responsibility8.png"
+    ].sample
+  end
+
   def help
     "Commands\n"
     ["all tasks",
@@ -101,5 +113,9 @@ class Tasker
      "<user> do <id>",
      "my tasks",
      "task help"].map{ |s| "  #{s}"}.join("\n")
+  end
+private
+  def nothing_urgent
+    "Nothing urgent. You can ask for random task though, say 'random task please'"
   end
 end
