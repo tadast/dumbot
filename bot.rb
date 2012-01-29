ac4a0acb2e77674210729afc0390d8833ad62465
Tadas Tamošauskas <tadas@pdfcv.com>
2012-01-29 16:18:18 +0000

    move task to lib. Reduce noise

--- bot.rb
+++ bot.rb
@@ -4,7 +4,7 @@ require 'net/http'
 require 'json'
 require 'cgi'
 require_relative 'bot_config.rb'
-require_relative 'tasker.rb'
+require_relative 'lib/tasker.rb'
 
 @config = BotConfig.new
 scamp = Scamp.new(:api_key => @config['api_key'], :subdomain => @config['subdomain'], :verbose => false)
